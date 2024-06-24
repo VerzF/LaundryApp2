261169586acf27b2bbc37a2bc655d9bd434470c2
Allendra Donny Irawan <105181500+VerzF@users.noreply.github.com>
2024-06-24 22:03:03 +0700

    resource tambahan

--- app/src/main/java/com/trisulaforce/laundryapp/model/DataProvider.kt
+++ app/src/main/java/com/trisulaforce/laundryapp/model/DataProvider.kt
@@ -78,133 +78,133 @@ object DataProvider{
         ),
         Layanan(
             13,
-            R.drawable.laundry1,
+            R.drawable.a13,
             "Gorden Cuci Kering",
             20000
         ),
         Layanan(
             14,
-            R.drawable.laundry1,
+            R.drawable.a14,
             "Gorden Cuci Setrika",
             35000
         ),
         Layanan(
             15,
-            R.drawable.laundry1,
+            R.drawable.a15,
             "Jas satuan cuci setrika",
             30000
         ),
         Layanan(
             16,
-            R.drawable.laundry1,
+            R.drawable.a16,
             "Jas 1 set",
             40000
         ),
         Layanan(
             17,
-            R.drawable.laundry1,
+            R.drawable.a17,
             "Tas kecil",
             10000
         ),
         Layanan(
             18,
-            R.drawable.laundry1,
+            R.drawable.a18,
             "Tas Besar",
             20000
         ),
         Layanan(
             19,
-            R.drawable.laundry1,
+            R.drawable.a19,
             "Satuan handuk Medium",
             10000
         ),
         Layanan(
             20,
-            R.drawable.laundry1,
+            R.drawable.a20,
             "Handuk large",
             12000
         ),
         Layanan(
             21,
-            R.drawable.laundry1,
+            R.drawable.a21,
             "Handuk Small",
             8000
         ),
         Layanan(
             22,
-            R.drawable.laundry1,
+            R.drawable.a22,
             "Bantal Guling small",
             25000
         ),
         Layanan(
             23,
-            R.drawable.laundry1,
+            R.drawable.a23,
             "Bantal",
             20000
         ),
         Layanan(
             24,
-            R.drawable.laundry1,
+            R.drawable.a24,
             "Jaket Kulit",
             25000
         ),
         Layanan(
             25,
-            R.drawable.laundry1,
+            R.drawable.a25,
             "Jaket Satuan",
             12000
         ),
         Layanan(
             26,
-            R.drawable.laundry1,
+            R.drawable.a26,
             "Kebaya Gantung",
             25000
         ),
         Layanan(
             27,
-            R.drawable.laundry1,
+            R.drawable.a27,
             "Kebaya 1 Set",
             35000
         ),
         Layanan(
             28,
-            R.drawable.laundry1,
+            R.drawable.a28,
             "Sprei Satuan Single",
             10000
         ),
         Layanan(
             29,
-            R.drawable.laundry1,
+            R.drawable.a29,
             "Sprei Double",
             15000
         ),
         Layanan(
             30,
-            R.drawable.laundry1,
+            R.drawable.a30,
             "Sprei Double++",
             18000
         ),
         Layanan(
             31,
-            R.drawable.laundry1,
+            R.drawable.a31,
             "Karpet",
             45000
         ),
         Layanan(
             32,
-            R.drawable.laundry1,
+            R.drawable.a32,
             "Selimut satuan Single",
             15000
         ),
         Layanan(
             33,
-            R.drawable.laundry1,
+            R.drawable.a33,
             "Selimut Double",
             20000
         ),
         Layanan(
             34,
-            R.drawable.laundry1,
+            R.drawable.a34,
             "Selimut Double++",
             25000
         ),
