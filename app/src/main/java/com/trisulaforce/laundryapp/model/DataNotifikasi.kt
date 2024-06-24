c018d9a1b3bc2b95337bb29c36257c202630b0f2
Allendra Donny Irawan <105181500+VerzF@users.noreply.github.com>
2024-06-24 10:36:39 +0700

    Notifikasi tambahan

---
+++ app/src/main/java/com/trisulaforce/laundryapp/model/DataNotifikasi.kt
@@ -0,0 +1,50 @@
+package com.trisulaforce.laundryapp.model
+
+import com.trisulaforce.laundryapp.R
+
+object DataNotifikasi {
+    val notifikasi :List<Notifikasi> = listOf(
+        Notifikasi(
+            id = 1,
+            R.drawable.konfirmasi,
+            "Sudah dikonfirmasi",
+            "Pesanan sudah dikonfirmasi",
+            "14/03/2024"
+        ),
+        Notifikasi(
+            id = 2,
+            R.drawable.konfirmasi,
+            "Siap dijemput",
+            "Pesanan siap dijemput",
+            "14/03/2024"
+        ),
+        Notifikasi(
+            id = 3,
+            R.drawable.konfirmasi,
+            "Sudah dikonfirmasi",
+            "Pesanan sudah dikonfirmasi",
+            "15/03/2024"
+        ),
+        Notifikasi(
+            id = 4,
+            R.drawable.konfirmasi,
+            "Siap dijemput",
+            "Pesanan siap dijemput",
+            "15/03/2024"
+        ),
+        Notifikasi(
+            id = 5,
+            R.drawable.konfirmasi,
+            "Sudah dikonfirmasi",
+            "Pesanan sudah dikonfirmasi",
+            "16/03/2024"
+        ),
+        Notifikasi(
+            id = 6,
+            R.drawable.konfirmasi,
+            "Siap dijemput",
+            "Pesanan siap dijemput",
+            "16/03/2024"
+        ),
+    )
+}
---
+++ app/src/main/java/com/trisulaforce/laundryapp/model/Notifikasi.kt
@@ -0,0 +1,11 @@
+package com.trisulaforce.laundryapp.model
+
+import java.io.Serializable
+
+data class Notifikasi (
+    val id: Long,
+    val image: Int = 0,
+    val title: String,
+    val text: String,
+    val tanggal: String
+    ) : Serializable
